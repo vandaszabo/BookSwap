9d1eec6e1239da25baa3996c21a3dec4e431b68b
vanda.szabo <msvandaszabo@gmail.com>
2024-03-22 00:38:12 +0100

    style: Change matches appearence

--- bookswap-frontend/src/Components/Matches.js
+++ bookswap-frontend/src/Components/Matches.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import { fetchUserById } from '../Utils/UserFunctions';
-import { Card, CardContent, CardMedia, Button } from '@mui/material';
+import { Card, CardContent, CardMedia, Button, Paper, Box } from '@mui/material';
 import { Typography } from '@mui/material';
-import {Grid} from '@mui/material';
+import { Grid } from '@mui/material';
 import CircleIcon from '@mui/icons-material/Circle';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
-export default function Matches({ userIds}) {
+export default function Matches({ userIds }) {
 
     const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -37,50 +37,61 @@ export default function Matches({ userIds}) {
             .then(() => console.log('Connection ID copied to clipboard'))
             .catch(err => console.error('Failed to copy connection ID: ', err));
     };
-    
+
 
     return (
         <React.Fragment>
-            <Typography color='primary' variant='h6' sx={{mt:4, mb: 2, borderBottom: '2px solid', borderColor: (theme) => theme.palette.secondary.light }}>Swap with Them</Typography>
+            <Typography color='primary' variant='h6' sx={{ mt: 4, mb: 2, borderBottom: '2px solid', borderColor: (theme) => theme.palette.secondary.light }}>Arrange Swap with Them</Typography>
             <Grid container spacing={4}>
-            {!loading && users && users.map((user, index) => (
-                <Grid
-                item
-                key={`${user.id}_${index}`}
-                xs={6}
-                sm={4}
-                md={3}
-                lg={3}>
-                <Card
-                    //onClick={handleView(user)}
-                    sx={{ height: '100%', display: 'flex', flexDirection: 'column', cursor: 'pointer' }}>
-                    <CardMedia
-                        component="div"
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            height: '200px',
-                            background: `url(${user.profileImage}) center/cover no-repeat`,
-                        }}>
-                    </CardMedia>
-                    <CardContent>
-                        <Typography variant="body1" component="div">
-                            {user.userName}
-                        </Typography>
-                        <Typography variant="body2" component="div">
-                            {user.city}
-                        </Typography>
-                        {user.connectionID && <>
-                            <CircleIcon sx={{ color: (theme) => theme.palette.secondary.green}} /> 
-                            <Typography>{user.connectionID}</Typography>
-                            <Button onClick={() => copyToClipboard(user.connectionID)}><ContentCopyIcon/></Button>
-                        </>}
-                        
-                    </CardContent>
-                </Card>
-                </Grid>
-            ))}
+                {!loading && users && users.map((user, index) => (
+                    <Grid
+                        item
+                        key={`${user.id}_${index}`}
+                        xs={6}
+                        sm={4}
+                        md={3}
+                        lg={3}>
+                        <Paper
+                            sx={{
+                                p: 2,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <div
+                                style={{
+                                    width: '150px',
+                                    height: '150px',
+                                    borderRadius: '50%',
+                                    overflow: 'hidden',
+                                    marginBottom: '16px',
+                                }}
+                            >
+                                <img
+                                    src={user.profileImage}
+                                    alt={user.userName}
+                                    style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                                />
+                            </div>
+                            <Typography variant="body1" component="div">
+                            {user.connectionID && <CircleIcon sx={{ color: (theme) => theme.palette.secondary.green }} />}
+                                {user.userName}
+                            </Typography>
+                            <Typography variant="body2" component="div">
+                                {user.city}
+                            </Typography>
+                            {user.connectionID && (
+                                <Box>
+                                    Copy ID
+                                    <Button sx={{cursor: 'pointer'}} onClick={() => copyToClipboard(user.connectionID)}>
+                                        <ContentCopyIcon />
+                                    </Button>
+                                </Box>
+                            )}
+                        </Paper>
+                    </Grid>
+                ))}
             </Grid>
         </React.Fragment>
     )
--- bookswap-frontend/src/Pages/Home.js
+++ bookswap-frontend/src/Pages/Home.js
@@ -13,7 +13,7 @@ const Home = ({ setSelectedPost }) => {
 
   const [popularBook, setPopularBook] = useState(null);
   const [loading, setLoading] = useState(true);
-  const {authUser} = useAuth();
+  const { authUser } = useAuth();
   const navigate = useNavigate();
 
   //*********-------Find most popular post-------*********//
@@ -41,49 +41,49 @@ const Home = ({ setSelectedPost }) => {
 
   return (
     <React.Fragment>
-    {!authUser ? (navigate('/login')):(
+      {!authUser ? (navigate('/login')) : (
         <Container component="main" maxWidth="lg" sx={{ mt: 4, mb: 4, textAlign: 'center' }}>
-        <Container sx={{ backgroundColor: (theme) => theme.palette.secondary.beige, minHeight: '100vh' }}>
-          <Box>
-            {popularBook && !loading &&
-              <Grid container spacing={4}>
-                {/* First Card */}
-                <Advert
-                  title={"Do you have a book you don't read anymore?"}
-                  description={"Upload it to our site and check out the offer."}
-                  buttonText={"Upload Your Book"}
-                  image={""}
-                  path={'/create'}
-                />
-                {/* Second Card */}
-                <Advert
-                  title={"Want to use it from your Phone?"}
-                  description={"Browse from anywhere"}
-                  buttonText={"Download the App"}
-                  image={"https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b0eb3857155439.5db0199390fe8.png"}
-                  path={'/'}
-                />
-                {/* Third Card */}
-                <Advert
-                  title={"Get a Book for Free!"}
-                  description={"Swap without delivery fee"}
-                  buttonText={"View Books in your City"}
-                  image={"https://www.scalebloom.com/wp-content/uploads/2022/07/blog220723webdes-1-1.png"}
-                  path={'/books'}
-                />
-                {/* Third Card */}
+          <Container sx={{ backgroundColor: (theme) => theme.palette.secondary.beige, minHeight: '100vh' }}>
+            <Box>
+              {popularBook && !loading &&
+                <Grid container spacing={4}>
+                  {/* Card 1*/}
+                  <Advert
+                    title={"Do you have a book you don't read anymore?"}
+                    description={"Upload it to our site and check out the offer."}
+                    buttonText={"Upload Your Book"}
+                    image={"https://th.bing.com/th/id/OIP.n3sAXxDyR0gM6xT1ZwmXdQHaE8?rs=1&pid=ImgDetMain"}
+                    path={'/create'}
+                  />
+                  {/* Card 2*/}
+                  <Advert
+                    title={"Most Popular"}
+                    description={`${popularBook.title} - ${popularBook.author}`}
+                    buttonText={"View"}
+                    image={popularBook.coverImage}
+                    path={'/post'}
+                  />
+                  {/* Card 3*/}
+                  <Advert
+                    title={"Get a Book for Free!"}
+                    description={"Swap without delivery fee"}
+                    buttonText={"View Books in your City"}
+                    image={"https://www.scalebloom.com/wp-content/uploads/2022/07/blog220723webdes-1-1.png"}
+                    path={'/books'}
+                  />
+                  {/* Card 4*/}
+                  <Advert
+                    title={"Want to use it from your Phone?"}
+                    description={"Browse from anywhere"}
+                    buttonText={"Download the App"}
+                    image={"https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b0eb3857155439.5db0199390fe8.png"}
+                    path={'/'}
+                  />
 
-                <Advert
-                  title={"Most Popular"}
-                  description={popularBook.title}
-                  buttonText={"View"}
-                  image={popularBook.coverImage}
-                  path={'/post'}
-                />
-              </Grid>}
-          </Box>
+                </Grid>}
+            </Box>
+          </Container>
         </Container>
-      </Container>
       )}
     </React.Fragment>
   );
